e807aa52e0974a3de2b1175c8c6182b895339519
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2021-05-04 20:31:32 +0200

    Deleted Unused modules

--- Project_7_Solution/Source/PowerUps.cpp
+++ Project_7_Solution/Source/PowerUps.cpp
@@ -2,28 +2,26 @@
 #include "PowerUps.h"
 #include "Collider.h"
 
-Power_Up::Power_Up() 
-{
-	position.SetToZero();
-};
 
-Power_Up::Power_Up(const Power_Up& p) : position(p.position), frameCount(p.frameCount), lifetime(p.lifetime)
-{
-};
+//Power_Up::Power_Up(const Power_Up& p) : position(p.position), frameCount(p.frameCount), lifetime(p.lifetime)
+//{
+//};
+//
+//Power_Up::~Power_Up()
+//{
+//	position.SetToZero();
+//
+//	if (collider != nullptr)
+//	{
+//		collider->pendingToDelete = true;
+//	}		
+//}
 
-Power_Up::~Power_Up()
-{
-	if (collider != nullptr)
-	{
-		collider->pendingToDelete = true;
-	}		
-}
-
-bool Power_Up::Update()
-{
-	if ()
-	{
-
-	}
-}
+//bool Power_Up::Update()
+//{
+//	//if ()
+//	//{
+//	//
+//	//}
+//}
 
